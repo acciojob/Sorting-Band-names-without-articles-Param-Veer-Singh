87b2120a800cce2907f17c501f8ed5435882121a
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-10-13 18:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'A Tajmahal'];
 
+
+function removeArticles(name){
+    let regex = /a|an|the/i;
+    // console.log(name);
+    // console.log(name.replace(regex,'').trim());
+    return name.replace(regex,'').trim();
+}
+
+function sortingNames(){
+    touristSpots.sort((a,b) => {
+        if(removeArticles(a) < removeArticles(b))return -1;
+        else return 1;
+    });
+}
+
+sortingNames();
+// console.log(touristSpots);
